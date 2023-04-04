0fdc2f486db94221d66ab039dea554b46da3dc0c
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-05 00:23:44 +0200

    console log

--- index.js
+++ index.js
@@ -51,8 +51,9 @@ app.get('/feed/:userId', jsonParser, async (req, res) => {
         // if date is equal or within the first and last dates of the week
         return date >= firstDayOfWeek && date <= lastDayOfWeek;
     }
-
-    if (type == "daily") posts = posts.filter(post => (new Date(post.createdAt).getFullYear()+'-'+new Date(post.createdAt).getDate()+'-'+(new Date(post.createdAt).getMonth()+1)) == (new Date().getFullYear()+'-'+new Date().getDate()+'-'+(new Date().getMonth()+1)))
+    console.log((new Date(posts[0].createdAt).getFullYear()+'-'+new Date(posts[0].createdAt).getDate()+'-'+(new Date(posts[0].createdAt).getMonth()+1)))
+    console.log(new Date())
+    if (type == "daily") posts = posts.filter(post => (new Date(post.createdAt).getFullYear() + '-' + new Date(post.createdAt).getDate() + '-' + (new Date(post.createdAt).getMonth() + 1)) == (new Date().getFullYear() + '-' + new Date().getDate() + '-' + (new Date().getMonth() + 1)))
     if (type == "weekly") posts = posts.filter(post => isDateInThisWeek(new Date(post.createdAt)))
   res.json(posts)
 })
