1dec1e5072ef4e912e051eb8353455d63025ab26
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-21 02:09:58 +0200

    [ADD] Badges and shop system

---
+++ functions/badges.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const badges_list = require('../badges.json')
+const { getUserByName } = require('./user')
+
+exports.getAllBadges = async function getAllBadges() {
+    return badges_list
+}
+
+exports.buyBadge = async function buyBadge(user, id) {
+    if (!id || !user) return { error: 'Arguments manquants' }
+    user = await getUserByName(user.name)
+    if (!user) return { error: 'User not found' }
+    let badge = badges_list.find(b => b.id == id)
+    if (!badge) return { error: 'Badge not found' }
+    if (user.profile.badges.find(b => b == id)) return { error: 'Badge déjà acheté' }
+    if (parseInt(user.profile.coins) < parseInt(badge.price)) return { error: 'Pas assez de coins' }
+    user.profile.badges.push(badge.id)
+    user = await prisma.profile.update({
+        where: { id: parseInt(user.id) },
+        data: {
+            badges: user.profile.badges,
+            coins: parseInt(user.profile.coins) - parseInt(badge.price)
+        },
+    }).catch(e => {
+        console.log(e)
+        return { error: 'Impossible d\'acheter le badge' }
+    })
+    console.log(user)
+    if (!user) return { error: 'User not found' }
+    return badge
+}
