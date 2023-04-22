34b56df5c3fbf02420f81a702a3d0badab1f8da9
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-22 22:52:40 +0200

    [ADD] Admin badges manager

--- functions/badges.js
+++ functions/badges.js
@@ -29,4 +29,21 @@ exports.buyBadge = async function buyBadge(user, id) {
     console.log(user)
     if (!user) return { error: 'User not found' }
     return badge
+},
+
+exports.updatebadges = async function updatebadges(user, new_badges) {
+    if (!user || !new_badges) return { error: 'Arguments manquants' }
+    if (!user) return { error: 'User not found' }
+    user = await prisma.profile.update({
+        where: { id: parseInt(user.id) },
+        data: {
+            badges: new_badges
+        },
+    }).catch(e => {
+        console.log(e)
+        return { error: 'Impossible de mettre à jour les badges' }
+    })
+    console.log(user)
+    if (!user) return { error: 'User not found' }
+    return user
 }
