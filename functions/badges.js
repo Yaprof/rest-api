cad69c5c8509a94631cdf9ab7fdd8d6d46b2c34e
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-04-23 20:02:51 +0200

    [FIX] Db userId for profile

--- functions/badges.js
+++ functions/badges.js
@@ -2,12 +2,14 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const badges_list = require('../badges.json')
 const { getUserByName } = require('./user')
+const { removeCoin } = require('./economy')
 
 exports.getAllBadges = async function getAllBadges() {
     return badges_list
 }
 
 exports.buyBadge = async function buyBadge(user, id) {
+    console.log('buying badge')
     if (!id || !user) return { error: 'Arguments manquants' }
     user = await getUserByName(user.name)
     if (!user) return { error: 'User not found' }
@@ -16,9 +18,9 @@ exports.buyBadge = async function buyBadge(user, id) {
     if (user.profile.badges.find(b => b == id)) return { error: 'Badge déjà acheté' }
     if (parseInt(user.profile.coins) < parseInt(badge.price)) return { error: 'Pas assez de coins' }
     user.profile.badges.push(badge.id)
-    console.log(user)
+    console.log(user.profile.badges)
     user = await prisma.profile.update({
-        where: { id: parseInt(user.id) },
+        where: { userId: parseInt(user.id) },
         data: {
             badges: user.profile.badges,
             coins: parseInt(user.profile.coins) - parseInt(badge.price)
@@ -27,6 +29,7 @@ exports.buyBadge = async function buyBadge(user, id) {
         console.log(e)
         return { error: 'Impossible d\'acheter le badge' }
     })
+    /* await removeCoin(user, badge.price) */
     console.log(user)
     if (!user) return { error: 'User not found' }
     return badge
@@ -36,7 +39,7 @@ exports.updatebadges = async function updatebadges(user, new_badges) {
     if (!user || !new_badges) return { error: 'Arguments manquants' }
     if (!user) return { error: 'User not found' }
     user = await prisma.profile.update({
-        where: { id: parseInt(user.id) },
+        where: { userId: parseInt(user.id) },
         data: {
             badges: new_badges
         },
